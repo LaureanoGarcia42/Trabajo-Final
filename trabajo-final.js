848c36f6b532e75e24af83e2b36c12186657af9d
LaureanoGarcia42 <123835658+LaureanoGarcia42@users.noreply.github.com>
2023-01-29 14:32:17 -0300

    Add files via upload

---
+++ trabajo-final.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+    document.getElementById("alerta").addEventListener("click", function() {
+        alert("La ultima actualizacion de la pagina fue el 29/01/2023");
+    })
+})
+
